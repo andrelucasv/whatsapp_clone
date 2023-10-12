1cb8462e3fcc981ef635e4a3ddccba316d26ade4
Andre Lucas <alucas748@gmail.com>
2023-10-12 02:45:57 -0300

    Criando interface de login

---
+++ lib/login_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+        ),
+        padding: const EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 32),
+                  child: Image.asset(
+                    "assets/images/logo.png", 
+                    width: 200, 
+                    height: 150,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: TextField(
+                    autofocus: true,
+                    keyboardType: TextInputType.emailAddress,
+                    style: const TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: "E-mail",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(32)
+                      )
+                    ),
+                  ),
+                ),
+                TextField(
+                    keyboardType: TextInputType.text,
+                    style: const TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: "Senha",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(32)
+                    )
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16, bottom: 10),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: const MaterialStatePropertyAll(Colors.green),
+                      padding: const MaterialStatePropertyAll(
+                        EdgeInsets.fromLTRB(32, 16, 32, 16
+                      )),
+                      shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32)
+                      ))
+                    ),
+                    child: const Text(
+                      "Entrar",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    )
+                  ),
+                ),
+                Center(
+                  child: GestureDetector(
+                    child: const Text(
+                      "Não tem conta? Cadastre-se!",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onTap: () {
+
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:whatsapp_clone/home_page.dart';
+//import 'package:whatsapp_clone/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:whatsapp_clone/login_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -12,13 +13,8 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  FirebaseFirestore db = FirebaseFirestore.instance;
-
-  await db.collection("usuarios")
-    .doc("001")
-    .set({"nome": "Andre"});
-
-  
+  // - Instância do Firestore
+  //FirebaseFirestore db = FirebaseFirestore.instance;
 
   runApp(const MyApp());
 }
@@ -28,9 +24,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Home(),
+    return MaterialApp(
+      home: const Login(),
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: const Color(0xff075E54),
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: const MaterialColor(
+            0xff075E54,
+            {
+              50: Color(0xff06554c),
+              100: Color(0xff064b43),
+              200: Color(0xff05423b),
+              300: Color(0xff043832),
+              400: Color(0xff042f2a),
+              500: Color(0xff032622),
+              600: Color(0xff021c19),
+              700: Color(0xff011311),
+              800: Color(0xff010908),
+              900: Color(0xff000000),
+            },
+          )
+        ).copyWith(
+          secondary: const Color(0xff25D366)
+        )
+      ),
     );
   }
 }
